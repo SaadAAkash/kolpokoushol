e6855789edf3848604296f12daeb4ad9fb681094
Mahmudul Alam <mahmudul@ccprotocol.xyz>
2023-09-21 16:46:42 +0600

    fix: contents and icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -56,6 +56,9 @@ export default defineConfig({
           'biotech',
           'cursor',
         ],
+        'grommet-icons': ['technology'],
+        iconamoon: ['music-1-fill'],
+        'simple-icons': ['moleculer'],
       },
     }),
 
